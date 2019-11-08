263e9e8f16e92b01de3a0dcba28d37b03edf7dc4
Romain Graux <romain.graux@student.uclouvain.be>
2019-11-08 16:37:22 +0100

    Rearange files v2

---
+++ src/stat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author : Romain Graux, Louis Delait, Lucas Delbecque, Paulin Martin, Louis Lejeune
+@date : Friday, 08 November 2019
+"""
+
+def main():
+    return 0
