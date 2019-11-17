2925eaecd6bdce5772b38bf4d1a9899154865736
Louis Delait <57060005+delaitl@users.noreply.github.com>
2019-11-17 13:40:27 +0100

    Update proba.py
    Synthèse du travail de Lucas

--- src/proba.py
+++ src/proba.py
@@ -2,8 +2,21 @@
 # -*- coding: utf-8 -*-
 """
 @author : Romain Graux, Louis Delait, Lucas Delbecque, Paulin Martin, Louis Lejeune
-@date : Friday, 08 November 2019
+@date :Sunday, 17 November 2019
 """
+#fonction calculant la distribution de P(N=j) avec 13 computations/Heure
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.special import factorial
+t=np.linspace(1,40,40)
+d=np.exp(-13)*np.power(13,t)/factorial(t)
+plt.plot(t,d,'o')
+plt.title("Statistical distribution of N")
+plt.xlabel("Computation in one hour")
+plt.ylabel("P(N)")
+plt.show()
+# With this distribution, E(x)=V(x)=13 ==> standart deviation= 3.60555
 
-def main():
-    return 0
+
+
+#fonction calculant la distribution de P()
