fb65cf7d5ca36f53d167871eabffcc7c9582f83e
MarKarl07 <49813487+MarKarl07@users.noreply.github.com>
2019-11-17 15:21:48 +0100

    Update proba.py
    ajouté les points deux et 3 mais sans être sur à 100/100 que ce soit bon

--- src/proba.py
+++ src/proba.py
@@ -16,6 +16,58 @@ plt.xlabel("Computation in one hour")
 plt.ylabel("P(N)")
 plt.show()
 # With this distribution, E(x)=V(x)=13 ==> standart deviation= 3.60555
+# On calcule la distribution conditionelle P(N1|n) en considérant n constant pour que lambda le soit aussi
+#calcul de la distribution de P(N1|360) :
+ 
+t=np.linspace(20,120,101)
+d=np.exp(-360*0.2)*np.power(360*0.2,t)/factorial(t)
+
+plt.plot(t,d,'o')
+plt.title("Statistical distribution of N1")
+plt.xlabel("Computation in one day")
+plt.ylabel("P(n1|360)")
+plt.show()
+
+#fonction 3d représentant toutes les autres (expérimentale):
+#t=np.linspace(20,120,101)
+#n=np.linspace(20,120,101)
+#d=np.zeros((101,101))
+#for i in range(0,101):
+#    d[i]=np.exp(-(n[i]*0.2))*np.power(n[i]*0.2,t)/factorial(t)
+    
+#Axes3D.plot(t, n, d)    
+#plt.plot(t,n,d,'o')
+#plt.title("Statistical distribution of N1")
+#plt.xlabel("Computation in one day")
+#plt.ylabel("P(n1|360)")
+#plt.show()
+
+#vaut +- 0.033
+# toutes les variables dépendent ainsi de n et la formule génerale de la distribution est np.exp(-(n*P(N1)))*np.power(n*P(N1),t)/factorial(t)
+# on trouve précisément la valeure de P(N1=64|n=360) avec 
+"""
+d=np.exp(-(360*0.2))*np.power(360*0.2,64)/factorial(64)
+print(d)
+
+# =0.031380522985433555 et la somme des points des ordonées de la courbe vaut bien 1 ce qui prouve mon génie
+# avec dès lors E(N1|n)=0.2*n=V(N1|n) et donc ==> standart deviation =  sqrt(0.2*n)
+"""
+
+#The expression of the exact joint distribution is given by : P(N1=j,N=n)=P(N1=j|N=n)*P(N=n)
+#plot de la pmf d'abord pour un n défini(13) ensuite en 3D:
+
+t=np.linspace(1,25,25)
+d1=np.exp(-13)*(13**(13))/factorial(13)
+#print(np.power(13,13))
+#print(13**(13))         ça donne pas la même chose Romaim Graux aide moi
+d2=np.exp(-(13*0.2))*np.power(13*0.2,t)/factorial(t)
+d=d1*d2
+plt.plot(t,d,'o')
+plt.title("pmf of the joint distribution of N1 and N")
+plt.xlabel("Computation in one hour")
+plt.ylabel("P(N1=j,N=n)")
+plt.show()
+#ça , ça donne les probabilités que n vale 13 et que N1 vale l'absisse , il manque plus qu'a faire un plot en 3D je suppose.
 
 
 
